dd44350ac4b1fb84f8f1ca69b5ae98d9b81d1963
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-16 09:44:04 +0800

    Done creating a list of substitutions that we will be used to decrypt the encrypted text

--- decrypt.py
+++ decrypt.py
@@ -5,6 +5,8 @@
 # Create a input for user
 encrypt_str = input("What is the text that you are trying to decrypt?: ")
 # Create a list of substitutions wherein this will be the basis of our program to decrypt the text
+def decrypted_str(encrypted_str):
+    decrypted_dict = {'*' : 'a', '&' : 'e', '#' : 'i', '+' : 'o', '!' : 'u'}
 # Create an empty list wherein it accepts the decrypted text
 # With the use of "For Loop", we command that every special text included in the input must be replace with their specific equivalent in the list of substitutions
 # Else just copy the char from the input then return the output
