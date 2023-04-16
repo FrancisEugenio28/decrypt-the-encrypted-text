6b618ebe089d22e8188d4257a81a33503f391dbd
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-16 09:12:26 +0800

    Pseudocode

---
+++ decrypt.py
@@ -0,0 +1,10 @@
+#Francis Niño A. Eugenio
+#BSCPE 1-5 | Object Oriented Programming
+#Problem 2: Lab Exercise 1
+
+# Create a input for user
+# Create a list of substitutions wherein this will be the basis of our program to decrypt the text
+# Create an empty list wherein it accepts the decrypted text
+# With the use of "For Loop", we command that every special text included in the input must be replace with their specific equivalent in the list of substitutions
+# Else just copy the char from the input then return the output
+# print the output
